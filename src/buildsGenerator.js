19020ffedb0f48590f09fc6c9395fe284d16b7da
Santy <shenseler@fi.uba.ar>
2025-05-09 11:39:48 -0300

    agrego otro modelo de edificio y agrego los edificios aleatoreamente

--- src/buildsGenerator.js
+++ src/buildsGenerator.js
@@ -1,13 +1,48 @@
 
 
-export function cilindricBuilding(height, radius) {
+export function cilindricBuilding(height, radius, angle1, angle2) {
 	return function (u, v, target) {
 
-		const x = radius * Math.cos(u*2*Math.PI) + 0.3 * Math.sin(v*Math.PI*2) + 0.5 * Math.sin(u*Math.PI*2);
-		const z = radius * Math.sin(u*2*Math.PI) +  0.3 * Math.sin(v*Math.PI*2) + 0.5 * Math.sin(u*Math.PI*2);
+		const x = radius * Math.cos(u*2*Math.PI) + angle1 * Math.sin(v*Math.PI*2) + angle2 * Math.sin(u*Math.PI*2);
+		const z = radius * Math.sin(u*2*Math.PI) + angle1 * Math.sin(v*Math.PI*2) + angle2 * Math.sin(u*Math.PI*2);
 		
 		const y = height * v;
 		
 		target.set(x, y, z);
 	};
 }
+
+export function rectangleBuilding(height, width, angle) {
+	return function (u, v, target) {
+		const face = Math.floor(u * 6);
+		const localU = (u * 6) % 1;
+	
+		let x = 0, y = v * height, z = 0;
+	
+		switch (face) {
+			case 0: // frontal
+				x = (localU - 0.5) * width;
+				z = width / 2;
+				break;
+			case 1: // derecha
+				x = width / 2;
+				z = (0.5 - localU) * width;
+				break;
+			case 2: // trasera
+				x = (0.5 - localU) * width;
+				z = -width / 2;
+				break;
+			case 3: // izquierda
+				x = -width / 2;
+				z = (localU - 0.5) * width;
+				break;
+
+		}
+	
+		const twistedX = Math.cos( v * angle) * x -  Math.sin( v * angle) * z;
+		const twistedZ =  Math.sin( v * angle) * x + Math.cos( v * angle) * z;
+
+		target.set(twistedX, y, twistedZ);
+	};
+}
+
--- src/cityGenerator.js
+++ src/cityGenerator.js
@@ -1,14 +1,14 @@
 import * as THREE from "three";
 import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
 
-import { cilindricBuilding } from "./buildsGenerator.js";
+import { cilindricBuilding , rectangleBuilding } from "./buildsGenerator.js";
 import { buildRoad } from "./roadGenerator.js";
+import { getRandomFloat, getRandomInt } from "./utils.js"
 
 
 const materials = {
   "ground": new THREE.MeshPhongMaterial ( {color: 0xF38E87, side: THREE.DoubleSide} ),
   "build": new THREE.MeshPhongMaterial  ( {color: 0xFFFFFF, side: THREE.DoubleSide} ),
-
 }
 
 export function cityGenerator() {
@@ -21,17 +21,34 @@ export function cityGenerator() {
   let ground = new THREE.Mesh( groundGeometry, materials["ground"] );
   ground.rotateX(Math.PI/2);
 
-  for(let i = 0; i < 2; i++){
-    for(let j = 0; j < 2; j++){
-      let builds = createBuilds(i*10-50, j*10-50);
-      //city.add(builds);
+  const notPos = {
+                1: [],
+                2: [5,6,7],
+                3: [4,5,6,7,8],
+                4: [3,8,9],
+                5: [2,4,5,6,9],
+                6: [2,4,5,6,9],
+                7: [2,4,5,6,9,10],
+                8: [3,4,5,6,9],
+                9: [6,7,8],
+                10: [],
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+      if (notPos[i].includes(j)) {
+        continue;
+      }
+
+      let builds = createBuilds((i-1) * 10 - 50, (j-1) * 10 - 50);
+      city.add(builds);
     }
   }
 
   city.add(ground);
 
+  city.add(buildRoad());
 
-  city.add(buildRoad())
   return city;
 }
 
@@ -39,17 +56,21 @@ function createBuilds(x, z){
 
   let neighborhood = new THREE.Group(); 
 
-  let geometry = new ParametricGeometry(cilindricBuilding(5, 1), 100, 100);
+  let geometry = new ParametricGeometry(getParametricFunction(), 100, 100);
 	let mesh = new THREE.Mesh(geometry, materials["build"]);
 
-  mesh.position.set(x+1, 0, z+1);
+  mesh.position.set(x+ getRandomFloat(2, 6), 0, z+getRandomFloat(2, 6));
   neighborhood.add(mesh);
   
-  let mesh2 = mesh.clone()
-
-  mesh2.position.set(x+5, 0, z+5);
-
-  neighborhood.add(mesh2);
-
   return neighborhood;
+}
+
+function getParametricFunction(){
+  const fn = getRandomInt(1,2);
+
+  switch (fn){
+    case 1: return cilindricBuilding( getRandomFloat(9, 15),  getRandomFloat(1, 2), getRandomFloat(0, 1), getRandomFloat(0, 1));
+    case 2: return rectangleBuilding( getRandomFloat(9, 15), getRandomFloat(2, 3), getRandomFloat(Math.PI, Math.PI*2));
+  }
+   
 }
